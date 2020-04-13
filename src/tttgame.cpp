7249d203dce38cc53d76d91a819733a0d3818ab0
Yue Liu <liu861@usc.edu>
2020-04-13 02:00:58 -0700

    working on genstates function

--- src/tttgame.cpp
+++ src/tttgame.cpp
@@ -1,5 +1,10 @@
 #include "tttgame.h"
 
+// Function: Constructor
+// Purpose: Creates an "empty board" in currentState --
+// A 3x3 grid of "empty" square states
+// Input: None
+// Returns: Nothing
 TicTacToeGame::TicTacToeGame()
 {
 	//set all squares to empty
@@ -12,12 +17,23 @@ TicTacToeGame::TicTacToeGame()
     }
 }
 
+// Function: getBoard
+// Purpose: Getter for the currentState member variable
+// Input: None
+// Returns: A GameState
 GameState TicTacToeGame::getBoard() const
 {
 	//getter for currentState member variable
 	return currentState;
 }
 
+
+// Function: setSquareState
+// Purpose: Sets the position (indicated by row and col) to
+// the indicated SquareState
+// Input: 2 integers representing the row/column to set
+//        A square state to set the r/c to
+// Returns: True to indicate the state was set successfully
 bool TicTacToeGame::setSquareState(unsigned short row, unsigned short col, GameState::SquareState state)
 {
 	//return true if SquareState has been set
@@ -28,6 +44,12 @@ bool TicTacToeGame::setSquareState(unsigned short row, unsigned short col, GameS
 	return false;
 }
 
+// Function: setSquareState
+// Purpose: Sets the position (indicated by the square number) to
+// the indicated SquareState
+// Input: An integer indicating the square number (upper left is #1)
+//        A square state to set square to
+// Returns: True to indicate the state was set successfully
 bool TicTacToeGame::setSquareState(unsigned short square, GameState::SquareState state)
 {
 	//if current SquareState has been set
@@ -97,6 +119,13 @@ bool TicTacToeGame::setSquareState(unsigned short square, GameState::SquareState
 	return false;
 }
 
+// Function: getBoard
+// Purpose: Determines if there's a winner and returns the symbol
+// Input: None
+// Returns: 'X' to indicate X wins
+//            'O' to indicate O wins
+//            'N' to indicate tie game
+//            ' ' to indicate no winner yet
 char TicTacToeGame::getWinner()
 {
     //let's check wins based on each player
@@ -104,53 +133,63 @@ char TicTacToeGame::getWinner()
     for(int i = 0; i < 3; i++)
     {
         //check horizontals for X win
-        if(currentState.mBoard[i][0] == GameState::X && currentState.mBoard[i][1] == GameState::X
-           && currentState.mBoard[i][2] == GameState::X)
+        if(currentState.mBoard[i][0] == GameState::X &&
+           currentState.mBoard[i][1] == GameState::X &&
+           currentState.mBoard[i][2] == GameState::X)
         {
             return 'X';
         }
         //check horizontals for O win
-        if(currentState.mBoard[i][0] == GameState::O && currentState.mBoard[i][1] == GameState::O
-           && currentState.mBoard[i][2] == GameState::O)
+        if(currentState.mBoard[i][0] == GameState::O &&
+           currentState.mBoard[i][1] == GameState::O &&
+           currentState.mBoard[i][2] == GameState::O)
         {
             return 'O';
         }
+        
         //check verticals for X win
-        if(currentState.mBoard[0][i] == GameState::X && currentState.mBoard[1][i] == GameState::X
-           && currentState.mBoard[2][i] == GameState::X)
+        if(currentState.mBoard[0][i] == GameState::X &&
+           currentState.mBoard[1][i] == GameState::X &&
+           currentState.mBoard[2][i] == GameState::X)
         {
             return 'X';
         }
         //check verticals for O win
-        if(currentState.mBoard[0][i] == GameState::O && currentState.mBoard[1][i] == GameState::O
-           && currentState.mBoard[2][i] == GameState::O)
+        if(currentState.mBoard[0][i] == GameState::O &&
+           currentState.mBoard[1][i] == GameState::O &&
+           currentState.mBoard[2][i] == GameState::O)
         {
             return 'O';
         }
     }
 
     //check diagonals for X win
-    if (currentState.mBoard[0][0] == GameState::X && currentState.mBoard[1][1] == GameState::X
-        && currentState.mBoard[2][2] == GameState::X)
+    if (currentState.mBoard[0][0] == GameState::X &&
+        currentState.mBoard[1][1] == GameState::X &&
+        currentState.mBoard[2][2] == GameState::X)
     {
         return 'X';
     }
-    else if (currentState.mBoard[0][2] == GameState::X && currentState.mBoard[1][1] == GameState::X
-        && currentState.mBoard[2][0] == GameState::X)
+    else if (currentState.mBoard[0][2] == GameState::X &&
+             currentState.mBoard[1][1] == GameState::X &&
+             currentState.mBoard[2][0] == GameState::X)
     {
         return 'X';
     }
     //check diagonals for O win
-    if (currentState.mBoard[0][0] == GameState::O && currentState.mBoard[1][1] == GameState::O
-        && currentState.mBoard[2][2] == GameState::O)
+    if (currentState.mBoard[0][0] == GameState::O &&
+        currentState.mBoard[1][1] == GameState::O &&
+        currentState.mBoard[2][2] == GameState::O)
     {
         return 'O';
     }
-    else if (currentState.mBoard[0][2] == GameState::O && currentState.mBoard[1][1] == GameState::O
-        && currentState.mBoard[2][0] == GameState::O)
+    else if (currentState.mBoard[0][2] == GameState::O &&
+             currentState.mBoard[1][1] == GameState::O &&
+             currentState.mBoard[2][0] == GameState::O)
     {
         return 'O';
     }
+    
     //now check for a tie, no winners and no empty spaces left
     int count = 0;
     for(int i = 0; i < 3; i++)
--- src/tttgameai.cpp
+++ src/tttgameai.cpp
@@ -1,34 +1,109 @@
 #include "tttgameai.h"
 
+// Function: GenStates
+// Purpose: Generates the game tree starting at the inputted root
+// Input: A GTNode pointer with the 1st state completed
+//        A boolean to indicate who's turn it is (true means it's X's turn)
+// Returns: Nothing
 void GenStates(GTNode* root, bool xPlayer)
 {
-	// TODO: IMPLEMENT
+	//iterate through all squares of root game state
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            //if a space is empty, generate a child node
+            if(root->mState.mBoard[i][j] == GameState::Empty)
+            {
+                //create new child node
+                GTNode* child = new GTNode;
+                
+                //set child's board to root's board
+                child->mState.mBoard[i][j] = root->mState.mBoard[i][j];
+                
+                //set empty position to player's symbol
+                if(xPlayer)
+                {
+                    child->mState.mBoard[i][j] = GameState::X;
+                }
+                else
+                {
+                    child->mState.mBoard[i][j] = GameState::O;
+                }
+                
+                //add child to vector of children
+                root->mChildren.push_back(child);
+                
+                //generate more child nodes for the child, using recursion
+                if(child->mState.mBoard[i][j] == GameState::Empty)
+                {
+                    if(xPlayer)
+                    {
+                        GenStates(child, false);
+                    }
+                    else
+                    {
+                        GenStates(child, true);
+                    }
+                }
+            }
+        }
+    }
 }
 
+// Function: GetScore
+// Purpose: Examines the inputted game board to determine a winner
+// Input: A game state representing a game board
+// Returns: Everything is relative to O, so...
+//        1.0 to indicate O wins on this board
+//        0.0 to indicate tie game
+//        -1.0 to indicate X wins on this board
 float GetScore(const GameState& state)
 {
 	// TODO: IMPLEMENT
 	return 0.0; // FIX RETURN VALUE
 }
 
+// Function: MinPlayer
+// Purpose: Determines the minimum score this branch (or leaf) can yield
+// Input: A GameTree node
+// Returns: The game score meaning...
+//        1.0 to indicate O wins on this board
+//        0.0 to indicate tie game
+//        -1.0 to indicate X wins on this board
 float MinPlayer(const GTNode* node)
 {
 	// TODO: IMPLEMENT
 	return 0.0; // FIX RETURN VALUE
 }
 
+// Function: MaxPlayer
+// Purpose: Determines the maximum score this branch (or leaf) can yield
+// Input: A GameTree node
+// Returns: The game score meaning...
+//        1.0 to indicate O wins on this board
+//        0.0 to indicate tie game
+//        -1.0 to indicate X wins on this board
 float MaxPlayer(const GTNode* node)
 {
 	// TODO: IMPLEMENT
 	return 0.0; // FIX RETURN VALUE
 }
 
+// Function: MinimaxDecide
+// Purpose: Determines which subtree leads to O winning
+// Input: A GameTree node
+// Returns: The GTNode* with the winning game state
 const GTNode* MinimaxDecide(const GTNode* root)
 {
 	// TODO: IMPLEMENT
 	return nullptr; // FIX RETURN VALUE
 }
 
+// Function: pickMove
+// Purpose: Decides which square the AI should select
+// Input: A game state with the current board's state
+// Returns: The square number (1 through 9) the AI selects
 unsigned pickMove(const GameState& board)
 {
 	// TODO: IMPLEMENT
